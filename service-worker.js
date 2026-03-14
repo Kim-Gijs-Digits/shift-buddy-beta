30fd6c6ff0a59e10c0637ab8029226054a101566
Kim Gijs <shifttap.app@gmail.com>
2026-03-14 21:25:50 +0100

    Update service worker cache version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'shift-tap-v2';
+const CACHE_VERSION = 'shift-tap-v3';
 const APP_SHELL_CACHE = `app-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 
